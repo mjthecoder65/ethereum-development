7ad2378146b04923f3be5407fe26f4e7ca024007
Michael <mjthecoder53161@gmail.com>
2024-11-04 13:22:46 +0900

    updated main.js

--- main.js
+++ main.js
@@ -9,27 +9,26 @@ const contract = new ethers.Contract("0x93f2A95d31F8FD24AD1B4B2550D879762B77ffF8
 
 const wallet = ethers.Wallet.fromPhrase(MNEMONIC_PHRASE).connect(provider);
 
-// async function sendEther() {
-//     try {
-//         const res = await wallet.sendTransaction({
-//             to: CONTRACE_ADDRESS,
-//             value: ethers.parseEther("0.28")
-//         });
-//         console.log("Transaction hash: ", res.hash);
-//         recept = await res.wait();
-//         console.log("Transaction mined in block: ", recept.blockNumber);
+async function sendEther() {
+    try {
+        const res = await wallet.sendTransaction({
+            to: CONTRACE_ADDRESS,
+            value: ethers.parseEther("0.28")
+        });
+        console.log("Transaction hash: ", res.hash);
+        recept = await res.wait();
+        console.log("Transaction mined in block: ", recept.blockNumber);
         
-//     } catch (err) {
-//         console.error(err);
-//     }
-// }
+    } catch (err) {
+        console.error(err);
+    }
+}
 
-// sendEther().then(async () => {
-//     const balance = await provider.getBalance(contract.address);
-//     console.log("Balance: ", balance.toString());
-// }).catch(err => {
-//     console.error(err);
-// })
+sendEther().then(message => {
+    console.log(message);
+}).catch(err => {
+    console.log(err);
+});
 
 
 contract.getBalance().then(balance => {
